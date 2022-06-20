d5d2dabf43926d8f2a4023e04bc769b2c01713b9
Heytemisan <temyicy@gmail.com>
2022-06-20 23:16:47 +0100

    slice ~ characterSlice

---
+++ src/redux/slices/characterReducer.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface ICharacterFav {
+    id: string
+    name: string
+}
+
+const characterSlice = createSlice({
+    name: "character",
+    initialState: <ICharacterFav[]>[],
+    reducers: {
+        setCharacterFav: (state, {payload}) => {
+            const { id, name } = payload;
+            const isFavAlready = state.find(
+                (set) => set.id === id && set.name === name,
+            );
+            if (isFavAlready) return state;
+            return [...state, { id, name },];
+        },
+        removeCharacterFav: (state, { payload }) => {
+            const { id, name } = payload;
+            return state.filter(
+                (state)=> state.name !== name || state.id !== id,
+            )
+        }
+    }
+});
+
+export const { setCharacterFav, removeCharacterFav} = characterSlice.actions;
+export default characterSlice.reducer;
