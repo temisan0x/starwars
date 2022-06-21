35a865fb4c3d96cabb95f15eee845cf3ff2ad009
Heytemisan <temyicy@gmail.com>
2022-06-21 06:21:24 +0100

    feat: useCallback memoized data

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 
 type Film = {
     title: string;
@@ -6,29 +6,29 @@ type Film = {
 };
 
 type Planet = {
-    title: string;
+    name: string;
     url: string;
 };
 
 type Vehicle = {
-    title: string;
+    name: string;
     url: string;
 };
 
 type Starship = {
-    title: string;
+    name: string;
     url: string;
 };
 
 type Species = {
-    title: string;
+    name: string;
     url: string;
 };
 
 type HomeWorld = {
-    title: string;
+    name: string;
     url: string;
-}
+};
 
 //attributes
 interface Character {
@@ -41,16 +41,65 @@ interface Character {
     homeworld: string;
     starships: string[];
     films: string[];
-    species: string[],
-    vehicles: string[],
+    species: string[];
+    vehicles: string[];
+    planets: string[];
     url: string;
-};
+}
 
 export function useCharacter(data: Character | undefined) {
+
     const [films, setFilms] = useState(<Film[]>[]);
     const [Planet, setPlanet] = useState(<Planet[]>[]);
     const [vehicles, setVehicles] = useState(<Vehicle[]>[]);
     const [starship, setStarship] = useState(<Starship[]>[]);
     const [species, setSpecies] = useState(<Species[]>[]);
-    const [homeworld, setHomeworld] = useState(<HomeWorld[]>[])
+    const [homeworld, setHomeworld] = useState<HomeWorld>({
+        name: "",
+        url: "",
+    });
+
+    const [loading, setLoading] = useState(true);
+    
+    //prevent run after every rendering ~ memoized
+    const getFilms = useCallback(async () => {
+        try {
+            data?.films.forEach(async (film) => {
+                const response = await fetch(film);
+                const filmData = await response.json();
+
+                setFilms((prevState) => {
+                    if (prevState.includes(filmData.title)) return prevState;
+                    return [
+                        ...prevState,
+                        {
+                            title: filmData.title,
+                            url: filmData.url,
+                        },
+                    ];
+                });
+            });
+        } catch {
+        } finally {
+            setLoading(false);
+        }
+    }, [data?.films]);
+
+    const getPlanets = useCallback(async () => {
+        try {
+            data?.planets.forEach(async (planet) => {
+                const response = await fetch(planet);
+                const planetData = await response.json();
+
+                setPlanet((prevState) => {
+                    if (prevState.includes(planetData.name)) return prevState;
+                    return [...prevState, { name: planetData.name, url: planetData.url }]
+                })
+            })
+        } catch {
+        } finally {
+            setLoading(false);
+        }
+    },[data?.planets])
 }
+
