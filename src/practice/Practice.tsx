69d4be63471f7ae52f2ef33517b6dbfa4591a64d
Heytemisan <temyicy@gmail.com>
2022-06-24 23:08:31 +0100

    leetcode: doubled for lloop

--- src/practice/Practice.tsx
+++ src/practice/Practice.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 const Practice = () => { 
-  const nums:Array<number> = [2,7,11,15];
-  // target = 10;
+  //nums = [2,7,11,15];
+  //target = 9;
+  //[2, 7] = 9,
+   //[0,1] = 9,
+  //i, j ~ double for loops
+  let twoSum = function (nums=[2,7,11,15],target= 9 ) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++){
+        if (nums[i] + nums[j] === target) {
+          return [i, j]
+        }
+      }
+    }
+  }
 
   return (
     <div>
-      {nums}
+      {twoSum()}
     </div>
   )
 }
