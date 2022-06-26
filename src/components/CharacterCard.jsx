a7503fba50cc3268bbdbfe56928699e39aff5fd4
Heytemisan <temyicy@gmail.com>
2022-06-26 10:18:10 +0100

    feat: imported characterSlice

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setCharacterFav, removeCharacterFav } from '../redux/slices/characterReducer'; 
+
+const CharacterCard = () => {
+    return (
+        <div>CharacterCard</div>
+    )
+}
+
+export default CharacterCard
