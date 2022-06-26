c54496a00bebb7aa7f192511d8979707834f3301
Heytemisan <temyicy@gmail.com>
2022-06-26 20:26:53 +0100

    chore: overwrite Iplanet.slice.ts

--- src/redux/slices/Planet.slice.ts
+++ src/redux/slices/Planet.slice.ts
@@ -11,11 +11,14 @@ const planets = createSlice({
     name: 'planets',
     initialState,
     reducers: {
-        setPanetFav: (state, action: PayloadAction<IPlanets>) => {
+        setPlanetFav: (state, action: PayloadAction<IPlanets>) => {
             const isFavAlready = state.find((set) => set.id === action.payload.id && set.name === action.payload.name)
             if (isFavAlready) return state;
-            return [...state, {id: action.payload.id, name: action.payload.name}]
+            return [...state, { id: action.payload.id, name: action.payload.name }]
         },
-        removePlanetFav: (state, action: PayloadAction<IPlanets>) => {return state.filter((set)=> set.id !== action.payload.id || set.name !== action.payload.name)}
+        removePlanetFav: (state, action: PayloadAction<IPlanets>) => { return state.filter((set) => set.id !== action.payload.id || set.name !== action.payload.name) }
     }
-})
+});
+
+export default planets.reducer;
+export const {setPlanetFav, removePlanetFav} = planets.actions
