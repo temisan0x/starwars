da8f634e861ecc337726b997e8d22f353a25fb11
Heytemisan <temyicy@gmail.com>
2022-07-02 01:51:49 +0100

    feat: config theme-color

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 //import { Palindrome } from "./practice/Palindrome";
 // import CharactersPage from './pages/CharactersPage';
--- src/components/PaginationBtn.ts
+++ src/components/PaginationBtn.ts
@@ -1,13 +1,16 @@
+import { transparentize } from 'polished';
 import styled from 'styled-components';
 
 interface IPaginationBtnProps {
     isActive?: boolean;
 }
+const color = '#fdb810';
 
 export const PaginationBtn = styled.button<IPaginationBtnProps>`
     width: 2.5rem;
     height: 2.5rem;
     border-radius: 4px;
     border: none;
+    background-color: ${transparentize(0.05, color)}
     
 `
