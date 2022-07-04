549a597b199f1140b5c640bceae9c0e3bf3f5dc4
Heytemisan <temyicy@gmail.com>
2022-07-04 19:36:03 +0100

    feat: inserted -- Footer & styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,10 @@ import { Container } from './styles';
 
 export const Footer = () => {
     return (
-        <Container>Footer</Container>
+        <Container>
+            <p>
+                Created by {''} <a href="https://github.com/">Temisan</a><br />
+            </p>
+        </Container>
     )
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,5 +2,22 @@ import styled from 'styled-components';
 
 
 export const Container = styled.footer`
-    
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p{
+        text-align: center;
+    }
+
+    a {
+        color : ${({ theme }) => theme.colors.primary.main};
+        transition: all 0.5s ease-in-out;
+        text-decoration:none;
+        
+        &:hover {
+            color:${({ theme }) => theme.colors.primary.dark}
+        }
+    }
 `
