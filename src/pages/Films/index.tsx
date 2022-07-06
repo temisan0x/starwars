5ca47daa40b6f1614b1b95ab84c0e7c2db40be86
Heytemisan <temyicy@gmail.com>
2022-07-06 17:55:12 +0100

    chore: bug fix 🐛

---
+++ src/pages/Films/index.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import { useSelector } from 'react-redux';
+import { Card } from '../../components';
+import { InputSearch } from '../../components/InputSearch';
+import { swapi } from '../../api/api';
+import { Loading } from '../../components/Loading';
+import { getUrlId } from '../../utils/getUrlId';
+import { SelectBtn } from '../../components';
+import { RootState } from '../../redux/rootReducer';
+import { Film } from '../../types/Film.types';
+
+
+
+export const Films = () => {
+    const [films, setFilms] = useState<Film[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [isFavSelected, setFavSelected] = useState(true);
+    const [inputSearch, setInputSearch] = useState<string>('');
+
+
+    const filmFav = useSelector((state: RootState) => state.films);
+
+    const getData = useCallback(async () => {
+        try {
+            const res = await swapi.get('films/');
+            const fetchedData = res.data;
+            setFilms(fetchedData.results);
+        }
+        catch {
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const getFilteredData = useCallback(async () => {
+        try { 
+            const res = await swapi.get(`films/?search=${inputSearch}`);
+            const fetchedData = await res.data;
+            setFilms(fetchedData.results);
+        } catch {
+        } finally {
+            setLoading(false);
+        }
+    }, [inputSearch]);
+
+    return (
+        <div>
+            films
+        </div>
+    )
+}
