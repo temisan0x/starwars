0eb611497411543f333d1e63cd1b0a7de624e172
Heytemisan <temyicy@gmail.com>
2022-07-23 08:29:26 +0100

    feat: created character slice

--- src/redux/slices/Character.slice.ts
+++ src/redux/slices/Character.slice.ts
@@ -1,8 +1,8 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-export interface ICharacterFav {
-    id: string;
-    name: string;
+export interface ICharacterFav{
+    id: string
+    name: string
 };
 
 const initialState: ICharacterFav[] = [];
@@ -11,26 +11,27 @@ const characterSlice = createSlice({
     name: "characters",
     initialState,
     reducers: {
-        setCharacterFav: (state, action:PayloadAction<ICharacterFav>) => {
+        setCharacterFav(state, action: PayloadAction<ICharacterFav>) {
             const isFavAlready = state.find(
-                (set) => set.id === action.payload.id
-                    && set.name === action.payload.name
-            );
+                (set) => set.id === action.payload.id  && set.name === action.payload.name
+            ) 
             if (isFavAlready) return state;
-            return [...state, { id: action.payload.id, name: action.payload.name }];
+            return [...state, {id: action.payload.id, name: action.payload.name}]
         },
-        removeCharacterFav: (state,action ) => {
+        removeCharacterFav(state, action) {
             return state.filter(
-                (set)=> set.name !== action.payload.name || set.id !== action.payload.id,
+                (set) => set.id !== action.payload.id || set.name !== action.payload.name
             )
         }
     }
-});
+})
 
-export const { setCharacterFav, removeCharacterFav} = characterSlice.actions;
-export default characterSlice.reducer;
 
+export const { setCharacterFav, removeCharacterFav } = characterSlice.actions
+export default characterSlice.reducer 
 /**
- * created actions 
- * 
+ * our most important concept of this phase is to create an avenue to check if a value is directly equal to the favourited state.
+ * firstly, we will first create an interface, which will connect to the stars wars api,
+ * our values will include the name and id; ... next, is to create a characterSlice using the createSlice function from reduxjs tool-kit,
+ * this will enable us to create bunch of reducers , initialState & name 
  */
