df07977a221488456fd4ba70c3b4606e45fbce82
Heytemisan <temyicy@gmail.com>
2022-07-24 09:51:06 +0100

    film slice

--- src/redux/slices/Film.slice.ts
+++ src/redux/slices/Film.slice.ts
@@ -3,26 +3,29 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface IFilmFav {
     id: string;
     title: string;
-}
+};
 
-const initialState: IFilmFav[] = []
+const initialState: IFilmFav[] = [];
 
 const filmSlice = createSlice({
-    name: "films",
+    name: "favcharacters",
     initialState,
     reducers: {
-        setFilmFav: (state,action: PayloadAction<IFilmFav>) => {
+        setFilmFav: (state, action: PayloadAction<IFilmFav>) => {
             const isFavAlready = state.find(
-                (set) => set.id === action.payload.id && set.title === action.payload.title,
-            );
+                (set) => set.id === action.payload.id &&
+                    set.title === action.payload.title
+            )
             if (isFavAlready) return state;
-            return [...state, { id:action.payload.id, title:action.payload.title }]
+            return [...state, { id: action.payload.id, title: action.payload.title }]
         },
-        removeFilmFav: (state, action: PayloadAction<IFilmFav>) => {
-            return state.filter((set) => set.title !== action.payload.title || set.id !== action.payload.title);
+        removeFilmFav: (state, action) => {
+            return state.filter(
+                (set) => set.id !== action.payload.id || set.title !== action.payload.name
+            )
         }
     }
 });
 
-export const { setFilmFav, removeFilmFav } = filmSlice.actions;
 export default filmSlice.reducer;
+export const { setFilmFav, removeFilmFav } = filmSlice.actions
