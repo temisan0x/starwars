1e2164c2b79aab8f88d02e8c27f9acec6dd51ea4
Heytemisan <temyicy@gmail.com>
2022-07-26 16:10:10 +0100

    chore: code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PageLayout } from "./components";
 import { Films } from "./pages/Films";
 import Home from "./pages/Home";
-//import { Palindrome } from "./practice/Palindrome";
-// import CharactersPage from './pages/CharactersPage';
 
 function App() {
   
